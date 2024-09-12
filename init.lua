57d0fa200b2d4f646d50949458fbcb276c762a02
Lachlan Miller <lachlan.miller.1990@outlook.com>
2024-09-13 09:14:18 +1000

    init

---
+++ init.lua
@@ -0,0 +1,194 @@
+vim.cmd("set expandtab")
+vim.cmd("set number")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  -- init.lua:
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    -- or                              , branch = '0.1.x',
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+
+  -- plugins/telescope.lua:
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    -- or                              , branch = '0.1.x',
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+  },
+  {
+    "nvimtools/none-ls.nvim",
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
+    "hrsh7th/nvim-cmp",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+  },
+  dependencies = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
+  {
+    "catppuccin/nvim",
+  },
+}
+
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+    plugins,
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  -- install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  -- checker = { enabled = true },
+})
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+
+require("lazy").setup({})
+
+local configs = require("nvim-treesitter.configs")
+configs.setup({
+  ensure_installed = { "lua", "python", "javascript", "typescript", "json" },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+require("mason").setup({})
+require("mason-lspconfig").setup({
+  ensure_installed = { "lua_ls", "pyright", "ts_ls" },
+})
+
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>")
+
+local null_ls = require("null-ls")
+
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.black,
+    -- null_ls.builtins.completion.spell,
+    -- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+  },
+})
+
+vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  command = "setlocal shiftwidth=2 tabstop=2",
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "py",
+  command = "setlocal shiftwidth=2 tabstop=2",
+})
+
+-- completions
+--
+local cmp = require("cmp")
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" }, -- For luasnip users.
+  }, {
+    { name = "buffer" },
+  }),
+})
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+require("lspconfig").pyright.setup({
+  capabilities = capabilities,
+})
+
+require("lspconfig").lua_ls.setup({
+  capabilities = capabilities,
+})
+
+require("lspconfig").ts_ls.setup({
+  capabilities = capabilities,
+})
+
+vim.api.nvim_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", {})
+
+-- setup must be called before loading
+vim.cmd("colorscheme catppuccin")
+
+require("catppuccin").setup({
+  flavour = "frappe", -- latte, frappe, macchiato, mocha
+  background = {     -- :h background
+    light = "latte",
+    dark = "frappe",
+  },
+})
